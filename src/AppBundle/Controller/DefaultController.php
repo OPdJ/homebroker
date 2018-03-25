7f26a3648ceed757a16097fa23a9048c32c8c320
Otavio Pereira <otajesus@bol.com.br>
2018-03-25 18:42:10 -0300

    Update DefaultController.php

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\Controller;
+namespace AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends AbstractController
+class DefaultController extends Controller
 {
   /**
     * @Route("/", name="home")
