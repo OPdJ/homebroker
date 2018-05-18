b7315881a0d9e111d535405c34d98b6a929d6e25
Jonatan Raimir <jonatansantos.prog@gmail.com>
2018-05-17 23:04:52 -0300

    pegando json de url

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Service\OrdemService;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -12,7 +13,12 @@ class DefaultController extends Controller
     * @Route("/", name="homepage")
     */
   public function index(){
+      $json = file_get_contents('https://viacep.com.br/ws/41750290/json/');
+      $obj = json_decode($json);
+      return new JsonResponse($obj);
+
       /** @var OrdemService $ordemController */
+      /*
       $ordemController = $this->container->get('app.ordem_service');
       $ordensCompra = $ordemController->getListOrdensForTipo();
       $ordensVenda = $ordemController->getListOrdensForTipo(2);
@@ -21,5 +27,6 @@ class DefaultController extends Controller
           'ordensCompra' => $ordensCompra,
           'ordensVenda' => $ordensVenda
       ]);
+      */
   }
 }
